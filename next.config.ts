c352c8b7d3b9a05b440be830d44ed38b5552f37e
mohamadrx996-coder <mohamadrx996@gmail.com>
2026-05-10 17:12:45 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: false,
   productionBrowserSourceMaps: false,
-  experimental: {
-    optimizePackageImports: ['z-ai-web-dev-sdk'],
-  },
-
 };
 
 export default nextConfig;
